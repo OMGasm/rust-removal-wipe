c07659fb6c57db715fef1f7ff49248fbeaa6ea78
OMGasm <OMGasm@users.noreply.github.com>
2023-06-21 17:22:35 +1000

    clippy

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{env::args, io, fs::{read_dir, DirEntry}};
 
 fn recurse_dir(dir: impl Into<String>) -> Result<Vec<DirEntry>, io::Error> {
     let mut files = Vec::new();
-    for entry in (read_dir(dir.into())?) {
+    for entry in read_dir(dir.into())? {
         if let Ok(entry) = entry {
             let meta = entry.metadata().expect("always metadata. no err");
             if meta.is_file() {
@@ -18,6 +18,6 @@ fn recurse_dir(dir: impl Into<String>) -> Result<Vec<DirEntry>, io::Error> {
 
 fn main() -> Result<(), io::Error>{
     let wipedir = args().next().expect("provide directory to erase");
-    let dir = recurse_dir(wipedir)?;
+    let _dir = recurse_dir(wipedir)?;
     Ok(())
 }
