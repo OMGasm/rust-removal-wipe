b5e3a254b7e03d1f6e07940a4202ab38000f6575
OMGasm <OMGasm@users.noreply.github.com>
2023-06-21 17:24:37 +1000

    more clippy

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use std::{env::args, io, fs::{read_dir, DirEntry}};
 
 fn recurse_dir(dir: impl Into<String>) -> Result<Vec<DirEntry>, io::Error> {
     let mut files = Vec::new();
-    for entry in read_dir(dir.into())? {
-        if let Ok(entry) = entry {
+    for entry in read_dir(dir.into())?.flatten() {
             let meta = entry.metadata().expect("always metadata. no err");
             if meta.is_file() {
                 files.push(entry);
@@ -11,7 +10,6 @@ fn recurse_dir(dir: impl Into<String>) -> Result<Vec<DirEntry>, io::Error> {
                 let mut deeper = recurse_dir(entry.path().to_string_lossy())?;
                 files.append(&mut deeper);
             }
-        }
     }
     Ok(files)
 }
